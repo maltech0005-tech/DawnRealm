4221230250d41457a46ae4f39523f674ad53e23c
anotida <maltech0005@gmail.com>
2025-12-23 23:30:20 +0200

    enemy ai is go
    just added enemy ai logic now when player is in range the enemy moves exactly in the direction of the player to attack, though i ended up removing the movement animations for enemy while trying to remove some bugs I will put them back soon. This got me the breakthrough I really needed for on the sprint

--- scenes/skeleton.gd
+++ scenes/skeleton.gd
@@ -1,34 +1,28 @@
 extends CharacterBody2D
 @onready var enemy : AnimatedSprite2D = $enemy_sprite
 
-const SPEED = 1000.0
+const SPEED = 30.0
 var is_moving = false
 var is_attack = false
 var left = Vector2(-1, 0)
 var right = Vector2(1, 0)
 var up = Vector2(0, -1)
 var down = Vector2(0, 1)
-var enemy_direction = down
-var player=null
+var enemy_direction = Vector2()
 var player_inrange = false
-var player_position
-
+var player_position = Vector2()
+var player = null
 func _ready() -> void:
-	var player = get_tree().get_first_node_in_group("player")
+	player = get_node("/root/game/player")
 
 func _physics_process(delta: float) -> void:
-	get_direction()
-	if is_moving:
+	if player:
 		if player_inrange:
 			attack()
 			velocity = enemy_direction*SPEED*delta
 	else:
-		if player_inrange:
-			attack()
-			velocity = enemy_direction*SPEED*delta
+		velocity = Vector2.ZERO
 	move_and_slide()
-	if player_inrange:
-		attack()
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -41,25 +35,6 @@ func _on_range_body_entered(body: Node2D) -> void:
 func _on_range_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_inrange = false
-
-	
-func get_direction():
-	if Input.is_action_pressed("move_left"):
-		enemy_direction = left
-		enemy.flip_h=true
-		is_moving=true
-	elif Input.is_action_pressed("move_right"):
-		enemy_direction = right
-		enemy.flip_h=false
-		is_moving=true
-	elif Input.is_action_pressed("move_up"):
-		enemy_direction = up
-		is_moving=true
-	elif Input.is_action_pressed("move_down"):
-		enemy_direction = down
-		is_moving=true
-	else:
-		is_moving = false
 		
 func motion_animation():
 	if enemy_direction==left:
@@ -100,5 +75,8 @@ func attack():
 		enemy.play("walking_back")
 	if enemy_direction==down:
 		enemy.play("walking_front")
-	enemy_direction=(player.global_position-global_position).normalized
+	if player:
+		enemy_direction=(player.global_position-global_position)
+	if not player:
+		print("no node player")
 	
